f9a9421b76d2dc11adcbd8e73506e3a74238f1d0
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-02-17 20:53:35 -0300

    Criação Class Input

---
+++ Aula_01/Exercicios/lib/input.class.php
@@ -0,0 +1,24 @@
+<?php
+
+    class Input {
+        public $type;
+        public $name;
+        public $id;
+        public $placeholder;
+        public $checked;
+        public $disabled;
+        public $class;
+        public $required;
+         
+        public function __construct($type, $name, $id,, $placeholder, $checked, $disabled, $class, $required) {
+            return '<input type ="'. $type .'" 
+                           name ="'. $name .'"
+                           id ="'. $id .'"
+                           checked ="'. $checked .'"
+                           disabled ="'. $disabled .'"
+                           class ="'. $class .'"
+                           required ="'. $required .'"
+                           placeholder ="'. $placeholder .'">';   
+        }
+    }
+?>
