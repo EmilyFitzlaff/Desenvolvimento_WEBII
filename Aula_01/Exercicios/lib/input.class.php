ee328520d790ac7789778f80199e56983ea40349
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-02-17 20:54:03 -0300

    Correção

--- Aula_01/Exercicios/lib/input.class.php
+++ Aula_01/Exercicios/lib/input.class.php
@@ -10,7 +10,7 @@
         public $class;
         public $required;
          
-        public function __construct($type, $name, $id,, $placeholder, $checked, $disabled, $class, $required) {
+        public function __construct($type, $name, $id, $placeholder, $checked, $disabled, $class, $required) {
             return '<input type ="'. $type .'" 
                            name ="'. $name .'"
                            id ="'. $id .'"
