bf3b7b933df0da92f2ab83194d8b0e8bc1e42829
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-02-17 21:31:49 -0300

    Correção

--- Aula_01/Exercicios/lib/input.class.php
+++ Aula_01/Exercicios/lib/input.class.php
@@ -1,5 +1,4 @@
 <?php
-
     class Input {
         public $type;
         public $name;
@@ -11,14 +10,25 @@
         public $required;
          
         public function __construct($type, $name, $id, $placeholder, $checked, $disabled, $class, $required) {
-            return '<input type ="'. $type .'" 
-                           name ="'. $name .'"
-                           id ="'. $id .'"
-                           checked ="'. $checked .'"
-                           disabled ="'. $disabled .'"
-                           class ="'. $class .'"
-                           required ="'. $required .'"
-                           placeholder ="'. $placeholder .'">';   
+            $this->type = $type;
+            $this->name = $name;
+            $this->id = $id;
+            $this->placeholder = $placeholder;
+            $this->checked = $checked;
+            $this->disabled = $disabled;
+            $this->class = $class;
+            $this->required = $required;
         }
+
+        public function __toString() {
+            return '<input type ="'. $this->type .'" 
+                           name ="'. $this->name .'"
+                           id ="'. $this->id .'"
+                           checked ="'. $this->checked .'"
+                           disabled ="'. $this->disabled .'"
+                           class ="'. $this->class .'"
+                           required ="'. $this->required .'"
+                           placeholder ="'. $this->placeholder .'">'; 
+        }  
     }
 ?>
