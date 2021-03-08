b090e6e860e1bf772aebad54b80cf243c61a883c
Emily Fitzlaff <fitzlaff.emily@gmail.com>
2021-03-08 12:33:07 -0300

    Ajuste no Fieldset

--- Aula_01/Exercicios/lib/fieldset.class.php
+++ Aula_01/Exercicios/lib/fieldset.class.php
@@ -3,6 +3,7 @@
     Class Fieldset {
         private $class;
         private $id;
+        private $lista = array();
 
         public function __construct($class, $id) {
             $this->class = $class;
@@ -14,9 +15,23 @@
             return $abertura;
         }
 
+        
         public function Fechamento() {
             $fechamento = "</fieldset>";
             return $fechamento;
+        }
+
+        public function addItens($array) {
+            $this->lista = $array;
+        }
+
+        public function __toString() {
+            $itens = $this->Abertura();
+            foreach ($this->lista as $objeto) {
+                $itens .= $objeto;
+            }
+            $itens .= $this->Fechamento();
+            return $itens;
         }
     }
 
